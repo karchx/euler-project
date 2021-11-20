2485637ad20068302858a01dcf8c899e1b6a64e7
KenethSandoval <kenethra.74@gmail.com>
2021-11-20 16:04:09 -0600

    feat: resolve problem 2

--- problem2/main.go
+++ problem2/main.go
@@ -2,30 +2,20 @@ package main
 
 import "fmt"
 
-var a []int
-
-func fib() func(int) int {
-	return func(x int) int {
-
-		if x >= 2 {
-			xf := a[x-1] + a[x-2]
-			a = append(a, xf)
-			return xf
-		}
-		a = append(a, x)
-		return (a[x])
-	}
-}
-
 func main() {
-	var limit int = 4000000
-  var result int
-	f := fib()
-	for i := 0; i <= limit; i++ {
-		if i%2 == 0 {
-      result = f(i)
-      fmt.Print(result)
+	var limit int = 4_000_000
+	var aux, fib uint64 = 1, 0
+	var init int
+	var result uint64
+
+	for init = 1; init <= limit; init++ {
+		aux += fib
+		fib = aux - fib
+		if fib <= 4_000_000 {
+			if fib%2 == 0 {
+				result += fib
+			}
 		}
 	}
-
+	fmt.Println(result)
 }
