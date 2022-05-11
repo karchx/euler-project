9b28627954b1db1c16b2b914c39c470f82bd7f06
KenethSandoval <kenethra.74@gmail.com>
2022-05-11 12:56:29 -0600

    feat: resolution problem 3 in golang

--- problem3/main.go
+++ problem3/main.go
@@ -1,14 +1,43 @@
 package main
 
-import "fmt"
-
-func primeFactor() func(int)int{
-  return func (n int)int {
-    return n
-  }
-}
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-  pf := primeFactor()
-  fmt.Print(pf(2))
+	n := largestPrimeFactor(600851475143)
+	fmt.Println(n)
+}
+
+func largestPrimeFactor(number int64) int64 {
+	var lastFactor, factor, maxFactor int64
+
+	if math.Mod(float64(number), 2) == 0 {
+		lastFactor = 2
+		number = number / 2
+
+		for i := 0; math.Mod(float64(number), 2) == 0; i++ {
+			number = number / 2
+		}
+	} else {
+		lastFactor = 1
+	}
+	factor = 3
+	maxFactor = int64(math.Sqrt(float64(number)))
+
+	for i := 0; number > 1 && factor <= int64(maxFactor); i++ {
+		if math.Mod(float64(number), float64(factor)) == 0 {
+			number = number / factor
+			lastFactor = factor
+			maxFactor = int64(math.Sqrt(float64(number)))
+		}
+		factor = factor + 2
+	}
+
+	if number == 1 {
+		return lastFactor
+	} else {
+		return number
+	}
 }
